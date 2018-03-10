d691ffc28436b8aac5fc539d43309a7e0b5efce3
ben_meier <ben.meier@oracle.com>
2018-03-10 17:04:57 +0200

    added travis ci

--- make_official.sh
+++ make_official.sh
@@ -2,49 +2,29 @@
 
 set -e
 
-# change these!
-GO_PROJECT=github.com/AstromechZA/spiro
-BINARY_NAME=spiro
-VERSION_NUM=$(cat VERSION)
+mkdir -p build/
+
+# add the git commit id and date
+VERSION="$(git describe --tags --dirty) (commit $(git rev-parse --short HEAD) @ $(git log -1 --date=short --pretty=format:%cd))"
 
 function buildbinary {
-    goos=$1
-    goarch=$2
+    export GOOS=$1
+    export GOARCH=$2
 
-    echo "Building official $goos $goarch binary"
+    echo "Building official ${GOOS} ${GOARCH} binary for version '${VERSION}'"
 
-    outputfolder="build/${goos}_${goarch}"
-    echo "Output Folder: $outputfolder"
-    mkdir -pv $outputfolder
+    go build -i -v -o "build/spiro-${GOOS}-${GOARCH}" -ldflags "-X \"main.Version=${VERSION}\""
 
-    export GOOS=$goos
-    export GOARCH=$goarch
-
-    govvv build -i -v -o "$outputfolder/$BINARY_NAME" $GO_PROJECT
-
-    ls -lh "$outputfolder/$BINARY_NAME"
-    file "$outputfolder/$BINARY_NAME"
+    echo "Done"
+    ls -l "build/spiro-${GOOS}-${GOARCH}"
+    file "build/spiro-${GOOS}-${GOARCH}"
     echo
+
+    unset GOOS
+    unset GOARCH
 }
 
-# build local 
-unset GOOS
-unset GOARCH
-govvv build $GO_PROJECT
-
-# do some tests
-rm -rfv demo/output/*
-./spiro demo/example1 demo/example1.yml demo/output 
-rm -rfv demo/output/*
-./spiro demo/example2 demo/example2.yaml demo/output 
-
-# build for mac
+# platform builds
 buildbinary darwin amd64
-
-# build for linux
 buildbinary linux amd64
-
-# zip up
-tar -czf $BINARY_NAME-${VERSION_NUM}.tgz -C build .
-ls -lh $BINARY_NAME-${VERSION_NUM}.tgz
-file $BINARY_NAME-${VERSION_NUM}.tgz
+buildbinary windows amd64
