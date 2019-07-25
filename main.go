03fcc1a73120e27c74dd5393288abf7781bab729
tantv-918 <52390215+tantv-918@users.noreply.github.com>
2019-07-25 09:09:12 +0700

    Update main.go

--- main.go
+++ main.go
@@ -364,7 +364,7 @@ func mainInner() error {
 	tf.RegisterTemplateFunction("unescape", Unescape)
 	tf.RegisterTemplateFunction("stringreplace", StringReplace)
 	tf.RegisterTemplateFunction("regexreplace", RegexReplace)
-	tf.RegisterTemplateFunction("add", A)
+	tf.RegisterTemplateFunction("add", Add)
 	return process(inputTemplate, &spec, outputDirectory, tf)
 }
 
